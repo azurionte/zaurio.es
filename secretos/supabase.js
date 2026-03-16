78ace8c591c00350520f655b1c269a4b8d026ca9
azurionte <dmnrobles@gmail.com>
2026-03-16 06:33:21 +0100

    Create supabase.js

---
+++ secretos/supabase.js
@@ -0,0 +1,3 @@
+const supabaseUrl = "https://adpjitccwwvlydrtvvqk.supabase.co"
+const supabaseKey = "sb_publishable_D8CL0HI8vLfD5L3g5ZgUGg_HOM6Ixdk"
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
