7ae402a6123e17b318d1b3ae60eb3ece78091376
azurionte <dmnrobles@gmail.com>
2026-03-16 06:49:32 +0100

    Delete secretos directory

--- secretos/supabase.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = "https://adpjitccwwvlydrtvvqk.supabase.co"
-const supabaseKey = "sb_publishable_D8CL0HI8vLfD5L3g5ZgUGg_HOM6Ixdk"
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
