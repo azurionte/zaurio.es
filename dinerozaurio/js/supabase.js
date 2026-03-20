c5c74f2c5351c42f884f688bf4d034cbafb3a7a2
azurionte <dmnrobles@gmail.com>
2026-03-20 03:35:48 +0100

    Update and rename dinerozaurio/js/supabase.js to herramientas/dinerozaurio/js/supabase.js

--- dinerozaurio/js/supabase.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'https://TU-PROYECTO.supabase.co';
-export const SUPABASE_ANON_KEY = 'TU_ANON_KEY';
-export const APP_BASE_URL = window.location.origin + '/dinerozaurio/';
---
+++ herramientas/dinerozaurio/js/supabase.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://adpjitccwwvlydrtvvqk.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_D8CL0HI8vLfD5L3g5ZgUGg_HOM6Ixdk';
+export const APP_BASE_URL = window.location.origin + '/dinerozaurio/';
