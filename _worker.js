5f0c81be430cd7dcbdd936b97dce1d911938e7ce
azurionte <dmnrobles@gmail.com>
2026-03-24 03:06:27 +0100

    cambios varios

--- _worker.js
+++ _worker.js
@@ -1,29 +1,28 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    const host = url.hostname;
+    let path = url.pathname;
 
-    const url = new URL(request.url)
-    const host = url.hostname
-    let path = url.pathname
+    if (path === "/" || path.endsWith("/")) {
+      path = `${path}index.html`;
+    }
 
-    if (path === "/") path = "/index.html"
-
-    let folder = ""
+    let folder = "";
 
     if (host === "miercoles.zaurio.es") {
-      folder = "/miercoles.zaurio.es"
+      folder = "/miercoles.zaurio.es";
+    } else if (host === "secretos.zaurio.es") {
+      folder = "/secretos.zaurio.es";
+    } else if (host === "herramientas.zaurio.es") {
+      folder = "/herramientas";
+    } else if (host === "zaurio.es" || host === "www.zaurio.es") {
+      folder = "";
+    } else {
+      return new Response("Host no reconocido", { status: 404 });
     }
 
-    if (host === "secretos.zaurio.es") {
-      folder = "/secretos.zaurio.es"
-    }
-
-    // dominio principal
-    if (host === "zaurio.es" || host === "www.zaurio.es") {
-      folder = ""
-    }
-
-    const assetUrl = new URL(folder + path, url.origin)
-
-    return env.ASSETS.fetch(new Request(assetUrl, request))
+    const assetUrl = new URL(folder + path, url.origin);
+    return env.ASSETS.fetch(new Request(assetUrl, request));
   }
-}
+};
