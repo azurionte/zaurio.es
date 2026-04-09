3dafd978aea93bb5856a24ef02e41e571be9b2e1
azurionte <dmnrobles@gmail.com>
2026-04-09 15:10:34 +0200

    Fix VelociChef mobile planner overflow

--- herramientas/velocichef/sw.js
+++ herramientas/velocichef/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "velocichef-shell-v12";
+const CACHE_NAME = "velocichef-shell-v13";
 const APP_URL = "/velocichef/";
 const ASSETS = [
   APP_URL,
--- herramientas/velocichef/version.js
+++ herramientas/velocichef/version.js
@@ -1,6 +1,6 @@
 window.__VELOCICHEF_VERSION__ = {
   major: 1,
   minor: 4,
-  build: "0904261500",
-  label: "1.4.0904261500",
+  build: "0904261508",
+  label: "1.4.0904261508",
 };
