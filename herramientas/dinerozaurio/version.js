fba85115ddaa99d133d0431f97434297747272c4
azurionte <dmnrobles@gmail.com>
2026-08-13 11:15:42 +0200

    Load DineroZaurio account context enhancements

--- herramientas/dinerozaurio/version.js
+++ herramientas/dinerozaurio/version.js
@@ -1,13 +1,19 @@
 window.__DINEROZAURIO_VERSION__ = {
   major: 2,
-  minor: 2,
-  build: "1308261045",
-  label: "2.2.1308261045",
+  minor: 3,
+  build: "1308261115",
+  label: "2.3.1308261115",
 };
 
 (() => {
-  const script = document.createElement('script');
-  script.src = './folder-mode-summary-v2.js?v=1308261045';
-  script.defer = true;
-  document.head.appendChild(script);
+  const scripts = [
+    './folder-mode-summary-v2.js?v=1308261115',
+    './folder-mode-enhancements-v3.js?v=1308261115'
+  ];
+  scripts.forEach(src => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.defer = true;
+    document.head.appendChild(script);
+  });
 })();
