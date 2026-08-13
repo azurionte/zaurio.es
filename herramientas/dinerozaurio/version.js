946da36031d1b33e3f4937c0bbc782f740d474d8
azurionte <dmnrobles@gmail.com>
2026-08-13 11:31:52 +0200

    Load folder balance fixes

--- herramientas/dinerozaurio/version.js
+++ herramientas/dinerozaurio/version.js
@@ -1,14 +1,15 @@
 window.__DINEROZAURIO_VERSION__ = {
   major: 2,
-  minor: 3,
-  build: "1308261115",
-  label: "2.3.1308261115",
+  minor: 4,
+  build: "1308261131",
+  label: "2.4.1308261131",
 };
 
 (() => {
   const scripts = [
-    './folder-mode-summary-v2.js?v=1308261115',
-    './folder-mode-enhancements-v3.js?v=1308261115'
+    './folder-mode-summary-v2.js?v=1308261131',
+    './folder-mode-enhancements-v3.js?v=1308261131',
+    './folder-mode-fixes-v4.js?v=1308261131'
   ];
   scripts.forEach(src => {
     const script = document.createElement('script');
