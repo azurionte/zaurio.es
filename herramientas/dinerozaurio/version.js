7017713a3737277c4ffd3c7b33f220c3ccc03785
azurionte <dmnrobles@gmail.com>
2026-08-13 10:10:04 +0200

    Load DineroZaurio folder mode summary fix

--- herramientas/dinerozaurio/version.js
+++ herramientas/dinerozaurio/version.js
@@ -1,6 +1,13 @@
 window.__DINEROZAURIO_VERSION__ = {
   major: 2,
-  minor: 1,
-  build: "1008261307",
-  label: "2.1.1008261307",
+  minor: 2,
+  build: "1308261009",
+  label: "2.2.1308261009",
 };
+
+(() => {
+  const script = document.createElement('script');
+  script.src = './folder-mode-summary-v2.js?v=1308261009';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
