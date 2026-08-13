e35ad2d2aea79e8f84988aba9b487c5631808e24
azurionte <dmnrobles@gmail.com>
2026-08-13 10:45:53 +0200

    Bump DineroZaurio summary UX version

--- herramientas/dinerozaurio/version.js
+++ herramientas/dinerozaurio/version.js
@@ -1,13 +1,13 @@
 window.__DINEROZAURIO_VERSION__ = {
   major: 2,
   minor: 2,
-  build: "1308261009",
-  label: "2.2.1308261009",
+  build: "1308261045",
+  label: "2.2.1308261045",
 };
 
 (() => {
   const script = document.createElement('script');
-  script.src = './folder-mode-summary-v2.js?v=1308261009';
+  script.src = './folder-mode-summary-v2.js?v=1308261045';
   script.defer = true;
   document.head.appendChild(script);
 })();
