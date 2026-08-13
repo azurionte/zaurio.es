72aa2b19c097af33218380cb139f3e4e89193eed
azurionte <dmnrobles@gmail.com>
2026-08-13 11:53:02 +0200

    Load DineroZaurio UI fixes v5

--- herramientas/dinerozaurio/version.js
+++ herramientas/dinerozaurio/version.js
@@ -1,15 +1,16 @@
 window.__DINEROZAURIO_VERSION__ = {
   major: 2,
-  minor: 4,
-  build: "1308261131",
-  label: "2.4.1308261131",
+  minor: 5,
+  build: "1308261152",
+  label: "2.5.1308261152",
 };
 
 (() => {
   const scripts = [
-    './folder-mode-summary-v2.js?v=1308261131',
-    './folder-mode-enhancements-v3.js?v=1308261131',
-    './folder-mode-fixes-v4.js?v=1308261131'
+    './folder-mode-summary-v2.js?v=1308261152',
+    './folder-mode-enhancements-v3.js?v=1308261152',
+    './folder-mode-fixes-v4.js?v=1308261152',
+    './ui-fixes-v5.js?v=1308261152'
   ];
   scripts.forEach(src => {
     const script = document.createElement('script');
