8309af91f48ec9a8b2f396c586417292b3bd8769
azurionte <dmnrobles@gmail.com>
2026-08-16 22:36:58 +0200

    Remove legacy precision test

--- herramientas/dinerozaurio/tests/precision.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const a=require('../finance/accounting-core.js');
-const assert=require('node:assert/strict');
-assert.equal(a.toCents(0.1+0.2),30);
-assert.equal(a.round2(17.979999999),17.98);
-assert.equal(a.fromCents(13264),132.64);
-console.log('precision ok');
