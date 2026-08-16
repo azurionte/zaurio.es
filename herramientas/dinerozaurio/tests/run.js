af673a8294e3c9a721cdac5c43c06be3d68204c4
azurionte <dmnrobles@gmail.com>
2026-08-16 22:37:12 +0200

    Remove legacy test runner

--- herramientas/dinerozaurio/tests/run.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const fs=require('node:fs'),path=require('node:path');
-const dir=__dirname;
-const files=fs.readdirSync(dir).filter(name=>name.endsWith('.test.js')).sort();
-for(const file of files)require(path.join(dir,file));
-console.log(`DineroZaurio: ${files.length} test files passed`);
