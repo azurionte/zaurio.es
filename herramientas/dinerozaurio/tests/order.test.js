5f972cbab17939ce9cc432d3cad980d986d59e49
azurionte <dmnrobles@gmail.com>
2026-08-16 22:36:45 +0200

    Remove legacy ordering test

--- herramientas/dinerozaurio/tests/order.test.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const a=require('../finance/accounting-core.js');
-const assert=require('node:assert/strict');
-assert.ok(a.ORDER.transfer<a.ORDER.event);
-assert.ok(a.ORDER.event<a.ORDER.observed);
-console.log('order ok');
