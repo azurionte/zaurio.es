cf0f452c896fa2fbdaf8287eb4185f460e634a08
azurionte <dmnrobles@gmail.com>
2026-08-16 22:37:39 +0200

    Remove legacy universal add test

--- herramientas/dinerozaurio/tests/universal-add.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const fs=require('node:fs'),path=require('node:path'),assert=require('node:assert/strict');
-const ui=fs.readFileSync(path.join(__dirname,'..','folder-mode-enhancements-v3.js'),'utf8');
-for(const token of ['dzUniversalMenu','expense','income','debt','goal','personal']) assert.ok(ui.includes(token),token);
-assert.ok(ui.includes('data-dz-add'));
-assert.equal(/prompt\(/.test(ui),false);
-console.log('universal-add ok');
