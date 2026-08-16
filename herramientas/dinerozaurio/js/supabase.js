d05fc1ecbe5cc50bb47718cd58f6c9bd49ed9087
azurionte <dmnrobles@gmail.com>
2026-08-16 22:32:43 +0200

    Remove legacy Supabase runtime

--- herramientas/dinerozaurio/js/supabase.js
+++
@@ -1,4 +0,0 @@
-﻿export const SUPABASE_URL = 'https://wsdtcsjkssvdqovdpxrq.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_kWc68mbD1KZg9eu38KVEAA_mDARNb-e';
-export const APP_BASE_URL = window.location.origin + '/dinerozaurio/';
-
