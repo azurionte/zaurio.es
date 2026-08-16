da5acbe4750a3eaddb083c014257aad7dc1bbee7
azurionte <dmnrobles@gmail.com>
2026-08-16 22:36:12 +0200

    Remove legacy free balance test

--- herramientas/dinerozaurio/tests/free-balance-projection.test.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const assert=require('node:assert/strict');
-const model=require('./general-bucket.test.js');
-assert.equal(model.secondary[0].general.label,'Saldo libre');
-assert.equal(model.secondary[0].general.projected,0);
-console.log('free-balance-projection ok');
