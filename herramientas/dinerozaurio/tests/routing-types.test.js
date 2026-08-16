35479bd2e940353fc597c21272022c239908f7ba
azurionte <dmnrobles@gmail.com>
2026-08-16 22:37:08 +0200

    Remove legacy routing types test

--- herramientas/dinerozaurio/tests/routing-types.test.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const a=require('../finance/accounting-core.js'),assert=require('node:assert/strict');
-const org={salaryAccountId:'p',assignments:{x:{accountId:'r',folderId:'f'}}};
-for(const type of ['Gasto','Deuda','Ingreso','Ahorro'])assert.deepEqual(a.assignmentFor({itemId:'x',type},org),{accountId:'r',folderId:'f'});
-console.log('routing-types ok');
