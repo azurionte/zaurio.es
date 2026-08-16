5d3a94dc11f8fb2f9daafb3670689d103d168c35
azurionte <dmnrobles@gmail.com>
2026-08-16 22:36:31 +0200

    Remove legacy test ids

--- herramientas/dinerozaurio/tests/ids.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-module.exports={bbva:'bbva',revolut:'revolut',food:'food',cooper:'cooper',fun:'fun',glovo:'glovo',netflix:'netflix',google:'google',debt:'debt',income:'income',saving:'saving'};
